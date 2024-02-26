39e5a9de346231762eea3643debcf381fe565809
ziad <ziadabdelaziz1995@gmail.com>
2024-02-26 23:36:36 +0200

    [ADD]: day18

--- day18/README.md
+++ day18/README.md
@@ -1,2 +1,65 @@
 # Interactivity, More Widgets & Theming Cont.
 26-2-2024
+
+### 1- Closing ModelBottomSheet
+* `Navigator.pop()`
+* the `Navigator` class provided by Flutter is help us to navigate between multiple screens.
+* calling the `pop` method will close the `ModalBottomSheet` if it's currently open.
+
+### 2- Date Picker
+* there are alot of applications that requires dealing with dates and registering data with it's date.
+```
+void _presentDatePicker() async {
+    final now = DateTime.now();
+    final pickedDate = await showDatePicker(
+        context: context,
+        firstDate: DateTime(now.year - 1),
+        initialDate: now,
+        lastDate: DateTime(now.year + 1),
+    );
+}
+```
+* this is how we can get can render a date by the function `showDatePicker` provided by Flutter.
+* you can access year, month and day of `DateTime` object.
+* `showDatePicker` returns a value of type `Future<DateTime>`.
+* we will talk later about `Future` object, but for now you can think of it as a value that will be returned in the future.
+* the `await` keyword is used whenever we want to store a `Future` object value in the future and it's only usable within an asyncronus function which is declared with `async` keyword (also we will talk about it later).
+
+### 3- Exclamation Mark (!)
+* putting `!` after a nullable variable we tell dart that this will never be null.
+* when we have a nullable variable passed to a function which required a non-nullable variable but we are sure this value won't be null when this piece of code is executed.
+
+### 4- Drop Down Button
+* you probably have seen an application where you choose from a drop down menu, to add this option in your Flutter app you can use `DropdownButton` widget.
+```
+List<String> cities = ['Damietta', 'Cairo', 'Aswan'];
+String selectedCity = 'Damietta';
+DropdownButton(
+    value: selectedCity,
+    items: cities.map((city) => DropdownMenuItem(
+        value: city,
+        child: Text(city)
+    )).toList(),
+    onChange (value){
+        setState((){
+            selectedCity = value ?? 'Damietta';
+        });
+    }
+)
+```
+* `items` parameter takes a value of type `List<DropdownMenuItem>`.
+* the `value` parameters takes the value that will be displayed on the button.
+* in the above code we used `map` function to map our values into a list of `DropdownMenuItem` values.
+* `onChange` paremter accepts a function which take an argument that is the selected value from the drop down menu<br>
+&nbsp; -> here we stored this value in `selectedCity` variable which will be displayed on the button.<br>
+&nbsp; -> we covered the case of the user not selecting a value by assigning the value `Damietta` if `value` is null.<br>
+
+---
+
+## Extras
+
+### 1- TextField Spacing
+* `TextField` takes as much space as it can, so be careful when placing it inside `Row` widget.
+
+### 2- String methods
+* `toUpperCase()`: you can use this method to display your text in Upper case.
--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -9,10 +10,27 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final pickedDate = await showDatePicker(
+      context: context,
+      firstDate: DateTime(now.year - 1),
+      initialDate: now,
+      lastDate: DateTime(now.year + 1),
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -20,7 +38,7 @@ class _NewExpenseState extends State<NewExpense> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(children: [
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         TextField(
           controller: _titleController,
           maxLength: 50,
@@ -29,6 +47,69 @@ class _NewExpenseState extends State<NewExpense> {
             label: Text('Title'),
           ),
         ),
+        SizedBox(
+          width: 150,
+          child: TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              label: Text('Amount'),
+              prefixText: '\$ ',
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        Row(children: [
+          DropdownButton(
+            value: _selectedCategory,
+            items: Category.values
+                .map((category) => DropdownMenuItem(
+                      value: category,
+                      child: Text(category.name.toUpperCase()),
+                    ))
+                .toList(),
+            onChanged: (category) {
+              setState(() {
+                _selectedCategory = category ?? Category.leisure;
+              });
+            },
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(_selectedDate == null
+                    ? 'No date selected'
+                    : formatter.format(_selectedDate!)),
+                IconButton(
+                  onPressed: _presentDatePicker,
+                  icon: const Icon(
+                    Icons.calendar_month,
+                    size: 30,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ]),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextButton(
+              onPressed: () {
+                _titleController.clear();
+                _amountController.clear();
+              },
+              child: const Text('Cancel'),
+            ),
+            const SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Save Expense'),
+            ),
+          ],
+        )
       ]),
     );
   }
