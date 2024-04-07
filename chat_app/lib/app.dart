07976f1f4b667f333164e36bd705900e1305592c
ziad <ziadabdelaziz1995@gmail.com>
2024-04-07 16:44:56 +0200

    [ADD]: day53

--- chat_app/lib/app.dart
+++ chat_app/lib/app.dart
@@ -1,4 +1,7 @@
-import 'package:chat_app/auth_screen.dart';
+import 'package:chat_app/ui/auth_screen/auth_screen.dart';
+import 'package:chat_app/ui/chat_screen.dart';
+import 'package:chat_app/ui/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -12,7 +15,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const AuthScreen()
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
--- chat_app/lib/ui/auth_screen/auth_screen.dart
+++ chat_app/lib/ui/auth_screen/auth_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:chat_app/ui/auth_screen/user_image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 final _firebase = FirebaseAuth.instance;
 
@@ -16,14 +20,20 @@ class _AuthScreenState extends State<AuthScreen> {
   var _isLogin = true;
   var _enteredEmail = '';
   var _enteredPassword = '';
+  File? _selectedImage;
 
   _submit() async {
     final formState = _formKey.currentState!;
     final isValid = formState.validate();
 
-    if (isValid) {
-      formState.save();
+    if (!isValid || !_isLogin && _selectedImage == null) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('make sure to fill all the fields correctly')));
+      return;
     }
+
+    formState.save();
 
     try {
       if (_isLogin) {
@@ -38,6 +48,16 @@ class _AuthScreenState extends State<AuthScreen> {
           password: _enteredPassword,
         );
         print(userCredentials);
+
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCredentials.user!.uid}.jpg');
+
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
+
+        print(imageUrl);
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {
@@ -81,6 +101,12 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
+                      if (!_isLogin)
+                        UserImagePicker(
+                          onPickImage: (pickedImage) {
+                            _selectedImage = pickedImage;
+                          },
+                        ),
                       TextFormField(
                         decoration: const InputDecoration(
                           labelText: 'Email Address',
